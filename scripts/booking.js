ca2257f3e9a1493cea12749dac77ed8a9919ee96
MyNameSame <samapranbmagar@gmail.com>
2023-12-09 23:06:44 -0700

    first commit

--- scripts/booking.js
+++ scripts/booking.js
@@ -21,26 +21,26 @@ const clearButton = document.getElementById('clear-button');
 // when the day buttons are clicked, we will apply the "clicked" class to that element, and update any other relevant variables. Then, we can recalculate the total cost.
 // added challenge: don't update the dayCounter if the same day is clicked more than once. hint: .classList.contains() might be helpful here!
 
-daysOfWeek.forEach(day => {
-    day.addEventListener('click', () => {
-        const whenClicked = day.classList.contains('clicked');
-        if (whenClicked) {
-            day.classList.remove('clicked');
-            numberOfDays = numberOfDays - 1;
-        } else {
-            day.classList.add('clicked');
-            numberOfDays = numberOfDays + 1;
-        }
-        updateCost();
-    });
+daysOfWeek.forEach(function(day) {
+	day.addEventListener('click', function() {
+		const whenClicked = day.classList.contains('clicked');
+		if (whenClicked) {
+			day.classList.remove('clicked');
+			numberOfDays = numberOfDays - 1;
+		} else {
+			day.classList.add('clicked');
+			numberOfDays = numberOfDays + 1;
+		}
+		updateCost();
+	});
 });
 
 
 /********* clear days *********/
 // when the clear-button is clicked, the "clicked" class is removed from all days, any other relevant variables are reset, and the calculated cost is set to 0.
 
-clearButton.addEventListener('click', () => {
-    daysOfWeek.forEach(day => {
+clearButton.addEventListener('click', function() {
+    daysOfWeek.forEach(function(day) {
         day.classList.remove('clicked');
         halfDay.classList.remove('clicked');
         fullDay.classList.add('clicked');
@@ -53,7 +53,7 @@ clearButton.addEventListener('click', () => {
 /********* change rate *********/
 // when the half-day button is clicked, set the daily rate to $20, add the "clicked" class to the "half" element, remove it from the "full" element, and recalculate the total cost.
 
-halfDay.addEventListener('click', () => {
+halfDay.addEventListener('click', function() {
     costPerDay = 20;
     halfDay.classList.add('clicked');
     fullDay.classList.remove('clicked');
@@ -63,7 +63,7 @@ halfDay.addEventListener('click', () => {
 // when the full-day button is clicked, the daily rate is set back to $35, the clicked class is added to "full" and removed from "half", and the total cost is recalculated.
 
 
-fullDay.addEventListener('click', () => {
+fullDay.addEventListener('click', function() {
     costPerDay = 35;
     fullDay.classList.add('clicked');
     halfDay.classList.remove('clicked');
